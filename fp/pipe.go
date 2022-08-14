72764d163cbe86561e0ba2657b305a54145c36cc
ItsMarcusLol <marcusaleclee2000@gmail.com>
2022-08-13 20:07:18 -0700

    Added case for passing nil as a function into a pipe

--- fp/pipe.go
+++ fp/pipe.go
@@ -5,6 +5,11 @@ func Pipe(args ...func(Obj) Obj) func(Obj) Obj {
 		if len(args) <= 0 {
 			return arg
 		}
+
+		if args[0] == nil {
+			return Pipe(args[1:]...)(nil)
+		}
+
 		return Pipe(args[1:]...)(args[0](arg))
 	}
 }
