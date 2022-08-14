0bbe77b88c4a9f9b87ad7e1c16cc2ca250dbbb25
ItsMarcusLol <marcusaleclee2000@gmail.com>
2022-08-13 20:45:41 -0700

    Added get function for geting key from object even nested object

---
+++ fp/get.go
@@ -0,0 +1,23 @@
+package fp
+
+func Get(keys ...string) func(Obj) Obj {
+	return func(arg Obj) Obj {
+		if len(keys) <= 0 {
+			return Obj{"result": nil} // TODO think about these nil cases
+		}
+
+		if len(keys) == 1 {
+			return Obj{"result": arg[keys[0]]}
+		}
+
+		switch arg[keys[0]].(type) {
+		case Obj:
+			return Get(keys[1:]...)(arg[keys[0]].(Obj))
+		case map[string]any:
+			// return Get(keys[1:]...)(arg[keys[0]].(map[string]any))
+			return Get(keys[1:]...)(arg[keys[0]].(Obj))
+		default:
+			return Obj{"result": nil} // TODO think about these nil cases
+		}
+	}
+}
