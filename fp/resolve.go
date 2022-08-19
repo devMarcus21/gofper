96372e105978faf8855f21018e5b946328b6b42b
devMarcus21 <marcusaleclee2000@gmail.com>
2022-08-19 15:02:33 -0700

    Added Resolve function

--- fp/resolve.go
+++ fp/resolve.go
@@ -1,5 +1,30 @@
 package fp
 
+func Resolve(arg Obj) func(Obj) Obj {
+	return func(state Obj) Obj {
+		return deepResolve(state)
+	}
+}
+
+func deepResolve(state Obj) Obj {
+	newState := Obj{}
+
+	for propName, prop := range state {
+		switch prop.(type) {
+		case nil:
+			continue // evict nil values
+		case Obj:
+			newState[propName] = deepResolve(prop.(Obj))
+		case map[string]any:
+			newState[propName] = deepResolve(Obj(prop.(map[string]any)))
+		default:
+			newState[propName] = prop
+		}
+	}
+
+	return newState
+}
+
 // Useful for appending new properties onto state while in pipe
 func ResolveInto(append Obj) func(Obj) Obj {
 	return func(state Obj) Obj {
@@ -15,21 +40,4 @@ func ResolveInto(append Obj) func(Obj) Obj {
 
 		return newState
 	}
-}
-
-func deepResolve(state Obj) Obj {
-	newState := Obj{}
-
-	for propName, prop := range state {
-		switch prop.(type) {
-		case Obj:
-			newState[propName] = deepResolve(prop.(Obj))
-		case map[string]any:
-			newState[propName] = deepResolve(Obj(prop.(map[string]any)))
-		default:
-			newState[propName] = prop
-		}
-	}
-
-	return newState
 }
